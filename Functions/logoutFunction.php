b044153ed98dd38bed4471e4214a6f9744feb5ab
Haotian Zhang <778631@ACDM.DS.SAIT.CA>
2019-02-04 14:54:57 -0700

    bug fixing

--- Functions/logoutFunction.php
+++ Functions/logoutFunction.php
@@ -1,8 +1,10 @@
 <?php
-  if(!isset($_SESSION)) 
-  { 
-      session_start(); 
-  } 
-session_destroy();
-header("Location: http://localhost/PHP-HAOTIAN-ZHANG/");
-?>
+      if (!isset($_SESSION)) {
+          session_start();
+      }
+      // $result;
+      // echo ("<script> $result=confirm ('are your sure?') </script>");
+   
+      session_destroy();
+      header("Location: http://localhost/PHP-HAOTIAN-ZHANG/");
+  ?>
